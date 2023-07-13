a440be24c2738381f349e8e79281c6a339765156
unknown <manikanta.official1408@gmail.com>
2023-07-13 09:48:39 +0530

    Student marks

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int Marks;
+                Console.WriteLine("Enter the Marks in pre");
+                Marks = int.Parse(Console.ReadLine());
+                if (Marks > 60)
+                {
+                    Console.WriteLine("Enter the Marks in final");
+                    int Finalmarks = int.Parse(Console.ReadLine());
+                    var result = Finalmarks > 55 ? "Selected" : "Fail in Final";
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("Fail in pre");
+                }
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("End of the code");
+                Console.ReadKey();
+            }
+            
+
+        }
+    }
+}
